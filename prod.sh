fb1f1306afaa4a09444642afaa585df76a805e60
Andrey Ivlev <pulemetik.ai@gmail.com>
2025-09-09 18:21:10 +0300

    upd prod script

--- prod.sh
+++ prod.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 # Use the same compose file and a dedicated project name for all actions
 docker compose -f docker-compose.yml -p ai-vtb down --remove-orphans || true
 docker compose -f docker-compose.yml -p ai-vtb build
-docker compose -f docker-compose.yml -p ai-vtb up
+docker compose -f docker-compose.yml -p ai-vtb up -d
